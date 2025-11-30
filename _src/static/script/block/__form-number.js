178c163ed7942f4eab861dc4d2f4e5d90feb8d08
kameisha.a <kameisha.a@promail.com>
2025-11-30 16:13:47 +0100

    fix

---
+++ _src/static/script/block/__form-number.js
@@ -0,0 +1,9 @@
+// const formNumberArray = document.querySelectorAll('[data-form-number]')
+// console.log(formNumberArray)
+//
+// if (formNumber) {
+//     const formNumberInput = document.querySelector('[data-form-number] input');
+//     const formNumberButtonUp = document.querySelector('[data-form-number] button:first-of-type');
+//     const formNumberButtonDown = document.querySelector('[data-form-number] button:last-of-type');
+//
+// }
