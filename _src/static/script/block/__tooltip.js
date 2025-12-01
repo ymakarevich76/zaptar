fdd2510ba69f173534ed436b083c9bacdc985feb
kameysha.yu <kameisha.yuliya@gmail.com>
2025-12-01 20:11:42 +0100

    fix tooltip light

--- _src/static/script/block/__tooltip.js
+++ _src/static/script/block/__tooltip.js
@@ -11,6 +11,19 @@ if(tooltips.length) {
   })
 }
 
+const tooltipsLight = document.querySelectorAll('[data-tippy-content-light]');
+
+if(tooltipsLight.length) {
+  tooltipsLight.forEach((tooltip, index) => {
+    const width = tooltip.dataset.widthTooltip ? Number(tooltip.dataset.widthTooltip) : 232
+
+    tippy(tooltip, {
+      theme: 'light',
+      maxWidth: width,
+    });
+  })
+}
+
 const tooltipsWithCode = document.querySelectorAll('[data-tippy-with-code-content]');
 
 if(tooltipsWithCode.length) {
@@ -27,11 +40,47 @@ if(tooltipsWithCode.length) {
     </ul>
     <br>
     Не принимаются к возврату: масла, жидкости и детали электрической группы.`
+
     tippy(tooltipWithCode, {
       content: tooltipContent,
       allowHTML: true,
       theme: 'default',
       maxWidth: width,
+    });
+  })
+}
+
+const tooltipsWithCodeLight = document.querySelectorAll('[data-tippy-with-code-content-light]');
+
+if(tooltipsWithCodeLight.length) {
+  tooltipsWithCodeLight.forEach((tooltipWithCode, index) => {
+
+    const tooltipContentFirst = `
+      <div class="tooltip-statistic">
+        <span class="tooltip-statistic__title">Показатели статистики выдачи</span>
+        Сроки указаны в рабочих днях до склада МСК. Выбирая поставщика, изучите статистику отгрузок.
+        <span class="tooltip-statistic__title">Индикаторы качества выдачи</span>
+        <p class="tooltip-statistic__indicator tooltip-statistic__indicator--1">90% - 100% выданных заказов</p>
+        <p class="tooltip-statistic__indicator tooltip-statistic__indicator--2">70% - 89% выданных заказов</p>
+        <p class="tooltip-statistic__indicator tooltip-statistic__indicator--3">40% - 69% выданных заказов</p>
+        <p class="tooltip-statistic__indicator tooltip-statistic__indicator--4">20% - 39% выданных заказов</p>
+        <p class="tooltip-statistic__indicator tooltip-statistic__indicator--5">0% - 19% выданных заказов</p>
+      </div>`
+
+    const tooltipContentSecond = `
+      <div class="tooltip-status">
+        <p><span class="tooltip-status__text">Качество выдачи:</span> <span class="tooltip-status__procent">92%</span></p>
+        <p><span class="tooltip-status__text">Склад:</span> Минимальная партия 10 шт</p>
+        <p><span class="tooltip-status__text">Дата обновления цены:</span> Сегодня</p>
+      </div>`
+
+    const tooltipContent = Number(tooltipWithCode.dataset.tippyWithCodeContentLight) === 1 ? tooltipContentFirst : tooltipContentSecond;
+
+    tippy(tooltipWithCode, {
+      content: tooltipContent,
+      allowHTML: true,
+      theme: 'light',
+      maxWidth: 265,
     });
   })
 }
